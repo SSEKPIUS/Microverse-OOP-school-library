d34524ea8302becd95a00f3dcf322e0f3e1e60cc
pius ssekweyama <ssekweyamapius@gmail.com>
2023-02-15 17:56:46 +0300

    fix linters

--- person.rb
+++ person.rb
@@ -5,23 +5,27 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age = 'Unknown', name,  parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  private
+
   def of_age?
     @age >= 18
   end
-  private :of_age?
+
+  public
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
   def correct_name
-    @name
+    name
   end
 end
